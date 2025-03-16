a6d3f551e2724a82482bac62cff7d5db69cd8add
AmyLau3653 <amylau3653@gmail.com>
2025-03-16 00:58:02 -0700

    cleanup output + add backward elimination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import copy
 
 
 def startAlg():
@@ -28,27 +29,46 @@ def startAlg():
     initial_accuracy = leave_one_out_cross_validation(data, list(range(1, num_features + 1)), None)
     print(f"Running nearest neighbor with all {num_features} features, using 'leave-one-out' evaluation, I get an accuracy of {initial_accuracy:.1f}%")
     
+    # calculate default rate
+    labels = data[:, 0]
+    # count number of each unique label, source: https://stackoverflow.com/questions/10741346/frequency-counts-for-unique-values-in-a-numpy-array
+    unique_labels, counts = np.unique(labels, return_counts=True)
+    # get the most common label
+    most_common_count = counts.max()
+    default_rate = most_common_count / len(labels)
+
+    print(f"Default rate: {default_rate:.1%}")
+
     # start run time
     start_time = time.time()
 
     # run the selected algorithm
     if choice == "1":
         best_features, best_accuracy  = forward_selection(data)
-    # elif choice == "2":
-    #     best_features = backward_elimination(data)
-    # else:
-    #     print("Invalid input. Please restart and select 1 or 2.")
-    #     return
+    elif choice == "2":
+        best_features, best_accuracy = backward_elimination(data)
+    else:
+        print("Invalid input. Please restart and select 1 or 2.")
+        return
     
     # end run time
     end_time = time.time()
     # total run time
     run_time = end_time - start_time
 
+    # change time to min, sec, and ms for a more detailed analysis of run time
+    # floor division for whole number minutes (source: https://www.geeksforgeeks.org/floor-division-in-python/)
+    minutes = int(run_time // 60)
+    # mod to get seconds
+    seconds = int(run_time % 60)
+    # convert to ms, then mod 1000 to get remaining ms
+    milliseconds = int((run_time * 1000) % 1000)
+
     # print best features and accuracy
     print(f"Finished search!! The best feature subset is {best_features}, which has an accuracy of {best_accuracy:.1f}%")
     # print run time
-    print(f"Algorithm runtime: {run_time:.2f} seconds")
+    # print(f"Algorithm runtime: {run_time:.2f} seconds")
+    print(f"cpu time: {minutes} min {seconds} sec {milliseconds} ms")
 
 # leave one out cross validation, based on matlab code provided by Professor Keogh in the Project 2 Briefing Slides
 # function accuracy = leave_one_out_cross_validation(data, current_set, feature_to_add)
@@ -119,9 +139,12 @@ def forward_selection(data):
             # if isempty(intersect(current_set_of_features, k))
             if k not in current_set_of_features:  # only consider features not already added
                 # disp(['--Considering adding the ', num2str(k), ' feature'])
-                print(f"--Considering adding feature {k}")
+                # print(f"--Considering adding feature {k}")
                 # accuracy = leave_one_out_cross_validation(data, current_set_of_features, k+1);
                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
+
+                features_being_tested = current_set_of_features + [k]
+                print(f"Using feature(s) {set(features_being_tested)} accuracy is {accuracy:.1f}%")
 
                 if accuracy > best_so_far_accuracy:      
                     best_so_far_accuracy = accuracy
@@ -132,11 +155,57 @@ def forward_selection(data):
             # current_set_of_features(i) = feature_to_add_at_this_level;
             current_set_of_features.append(feature_to_add_at_this_level)
             # disp(['On level ', num2str(i), ' I added feature ', num2str(feature_to_add_at_this_level), ' to current set'])
-            print(f"On level {i+1}, added feature {feature_to_add_at_this_level} to current set: {current_set_of_features}")
+            # print(f"On level {i+1}, added feature {feature_to_add_at_this_level} to current set: {current_set_of_features}")
+
+            print(f"\nFeature set {set(current_set_of_features)} was best, accuracy is {best_so_far_accuracy:.1f}%\n")
 
             if best_so_far_accuracy > best_accuracy:  # update best accuracy so far
                 best_accuracy = best_so_far_accuracy
                 best_feature_set = current_set_of_features.copy()
+
+    return best_feature_set, best_accuracy 
+
+# start with full feature set instead of empty set
+def backward_elimination(data):
+    num_features = data.shape[1] - 1
+
+    # start with all features
+    current_set_of_features = list(range(1, num_features + 1))
+
+    # initial best
+    best_accuracy = leave_one_out_cross_validation(data, current_set_of_features, None)
+
+    # initial best is all features
+    best_feature_set = current_set_of_features.copy()
+
+    print("\nBeginning Backward Elimination\n")
+
+    for i in range(num_features - 1):
+        print(f"Backward elimination of features from {current_set_of_features}")
+        feature_to_remove_at_this_level = None
+        best_so_far_accuracy = best_accuracy
+
+        for k in range(len(current_set_of_features)):   
+            potential_feature_set = current_set_of_features.copy()
+            eliminated_feature = potential_feature_set.pop(k)
+
+            accuracy = leave_one_out_cross_validation(data, potential_feature_set, None)
+            print(f"Removing feature {eliminated_feature} accuracy is {accuracy:.1f}%")
+
+            if accuracy > best_so_far_accuracy:      
+                best_so_far_accuracy = accuracy
+                feature_to_remove_at_this_level = eliminated_feature
+
+
+        if feature_to_remove_at_this_level is not None:
+            current_set_of_features.remove(feature_to_remove_at_this_level)
+
+            if best_so_far_accuracy > best_accuracy:
+                best_accuracy = best_so_far_accuracy
+                best_feature_set = current_set_of_features.copy()
+                print(f"Removing feature {feature_to_remove_at_this_level} was best, accuracy is {best_so_far_accuracy:.1f}%\n")
+            else:
+                print("Accuracy dropped! Continuing search...")
 
     return best_feature_set, best_accuracy 
         
