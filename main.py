0393e9e7e5e152db41a40349cf03e9e493588460
AmyLau3653 <amylau3653@gmail.com>
2025-03-16 01:47:31 -0700

    remove matlab code used for reference

--- main.py
+++ main.py
@@ -54,22 +54,20 @@ def startAlg():
     # end run time
     end_time = time.time()
     # total run time
-    run_time = end_time - start_time
+    runtime = end_time - start_time
 
-    # change time to min, sec, and ms for a more detailed analysis of run time
-    # floor division for whole number minutes (source: https://www.geeksforgeeks.org/floor-division-in-python/)
-    minutes = int(run_time // 60)
-    # mod to get seconds
-    seconds = int(run_time % 60)
-    # convert to ms, then mod 1000 to get remaining ms
-    milliseconds = int((run_time * 1000) % 1000)
+    # runtime in minutes for the small dataset
+    minutes = runtime / 60
+    # runtime in hours for the large dataset
+    hours = runtime / 3600
 
     # print best features and accuracy
     print(f"Finished search!! The best feature subset is {best_features}, which has an accuracy of {best_accuracy:.1f}%")
-    # print run time
-    # print(f"Algorithm runtime: {run_time:.2f} seconds")
-    print(f"cpu time: {minutes} min {seconds} sec {milliseconds} ms")
-
+    # print runtime
+    print(f"Runtime: {runtime:.2f} seconds")
+    print(f"Runtime: {minutes:.2f} minutes")
+    print(f"Runtime: {hours:.2f} hours")
+    
 # leave one out cross validation, based on matlab code provided by Professor Keogh in the Project 2 Briefing Slides
 # function accuracy = leave_one_out_cross_validation(data, current_set, feature_to_add)
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
@@ -78,38 +76,25 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
     
     feature_set_to_use = current_set if feature_to_add is None else current_set + [feature_to_add]
 
-    # for i = 1 : size(data,1)  % Loop through each instance
     for i in range(num_instances): 
-        # object_to_classify = data(i,2:end);  % Extract feature values (ignoring class label)
-        object_to_classify = data[i, feature_set_to_use]  # get features, ignore class label\
-        # label_object_to_classify = data(i,1);  % Extract class label
+        object_to_classify = data[i, feature_set_to_use]  # get features, ignore class label
         label_object_to_classify = data[i, 0]  # get class label
         
-        # nearest_neighbor_distance = inf;  % Initialize min distance as infinity
         nearest_neighbor_distance = float('inf')
-        # nearest_neighbor_location = inf;
         nearest_neighbor_label = None
         
-        # for k = 1 : size(data,1)  % Loop through all other instances
         for k in range(num_instances): 
-            # if k ~= i  % Exclude the test instance itself
             if k != i: 
-                # distance = sqrt(sum((object_to_classify - data(k,2:end)).^2));  % Compute Euclidean distance
                 # source: https://www.geeksforgeeks.org/calculate-the-euclidean-distance-using-numpy/
                 distance = np.linalg.norm(object_to_classify - data[k, feature_set_to_use])  # get euclidean distance
                 
-                # if distance < nearest_neighbor_distance  % Update nearest neighbor
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
-                    # nearest_neighbor_location = k;
-                    # nearest_neighbor_label = data(nearest_neighbor_location,1);  % Get class of nearest neighbor
                     nearest_neighbor_label = data[k, 0]
 
         if label_object_to_classify == nearest_neighbor_label:
-            # number_correctly_classfied = number_correctly_classfied + 1;  % Count correct predictions
             num_correctly_classified += 1
 
-    # accuracy = number_correctly_classfied / size(data,1);  % Compute accuracy
     accuracy = (num_correctly_classified / num_instances) * 100
 
     return accuracy
@@ -126,21 +111,13 @@ def forward_selection(data):
 
     print("\nBeginning Forward Selection\n")
 
-    # for i = 1: size(data, 2) -1
     for i in range(num_features):
-        # disp(['On the ', num2str(i), 'th level of the search tree'])
         print(f"On the {i+1}th level of the search tree")
-        # feature_to_add_at_this_level = [];
         feature_to_add_at_this_level = None
         best_so_far_accuracy = 0
 
-        # for k = 1: size(data, 2) - 1
         for k in range(1, num_features + 1):
-            # if isempty(intersect(current_set_of_features, k))
             if k not in current_set_of_features:  # only consider features not already added
-                # disp(['--Considering adding the ', num2str(k), ' feature'])
-                # print(f"--Considering adding feature {k}")
-                # accuracy = leave_one_out_cross_validation(data, current_set_of_features, k+1);
                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
 
                 features_being_tested = current_set_of_features + [k]
@@ -152,10 +129,7 @@ def forward_selection(data):
 
 
         if feature_to_add_at_this_level is not None:  # add the best feature found
-            # current_set_of_features(i) = feature_to_add_at_this_level;
             current_set_of_features.append(feature_to_add_at_this_level)
-            # disp(['On level ', num2str(i), ' I added feature ', num2str(feature_to_add_at_this_level), ' to current set'])
-            # print(f"On level {i+1}, added feature {feature_to_add_at_this_level} to current set: {current_set_of_features}")
 
             print(f"\nFeature set {set(current_set_of_features)} was best, accuracy is {best_so_far_accuracy:.1f}%\n")
 
